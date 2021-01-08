aa0a6b2bb7878c90f343589b7f89b28ec1c97105
Will <wfmrohde@gmail.com>
2021-01-08 13:56:33 +0000

    vision node now publishes a pose_array of weed targets

--- vision/vision_node.py
+++ vision/vision_node.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python
 
-import rospy
+#python libraries
 import sys
 import numpy as np
 import cv2
-from std_msgs.msg import String
-from sensor_msgs.msg import Image
-import image_geometry
+
+#ROS libraries
+import rospy, image_geometry, tf
+
+#ROS messages
+from sensor_msgs.msg import Image, CameraInfo
 from cv_bridge import CvBridge, CvBridgeError
+from geometry_msgs.msg import Pose, PoseStamped, PoseArray
 
 def mask_crop(hsv_image):
     crop_high = (160,200,200)
@@ -78,28 +82,80 @@ def connected_components_from_image(image,filter_small_connected_components=True
     return num_labels,labels_im,stats,centroids
 
 class image_converter:
+    camera_model = None
+    cv_image = None
+    
     def __init__(self):
+        #publishers
         self.image_pub = rospy.Publisher("{}/opencv_image".format(robot_name),Image,queue_size=0)
-        self.green_detected_pub = rospy.Publisher("{}/green_detected".format(robot_name),String,queue_size=0) 
-        self.image_sub = rospy.Subscriber("/{}/kinect2_camera/hd/image_color_rect".format(robot_name),Image,self.callback)
+        self.weed_pose_array_pub = rospy.Publisher("{}/weed_pose_array".format(robot_name),PoseArray,queue_size=0)
+
+        #subscribers
+        self.image_sub = rospy.Subscriber("/{}/kinect2_camera/hd/image_color_rect".format(robot_name),Image,self.image_callback)
+        self.camera_info_sub = rospy.Subscriber('/{}/kinect2_camera/hd/camera_info'.format(robot_name),CameraInfo, self.camera_info_callback)
+        
+        self.tf_listener = tf.TransformListener()
         self.bridge = CvBridge()
 
-    def callback(self,data):
+    def camera_info_callback(self, data):
+        self.camera_model = image_geometry.PinholeCameraModel()
+        self.camera_model.fromCameraInfo(data)
+        self.camera_info_sub.unregister() #Only subscribe once
+
+    def image_callback(self,data):
         try:
-            cv_image = self.bridge.imgmsg_to_cv2(data,"bgr8")
+            self.cv_image = self.bridge.imgmsg_to_cv2(data,"bgr8")
+            cv_image = self.cv_image
+            print("received image")
         except CvBridgeError as e:
             print(e)
+
+        #TODO split image_callback from image_processing when changing to service.
+
+        if self.camera_model is None:
+            return #TODO add exception or similar to aid debugging this
+        if self.cv_image is None:
+            return #TODO add exception or similar to aid debugging this
         
+        print("processing image")
         weed_pixel_coords, crop_pixel_coords = self.process_image_colour_kmeans(cv_image)
+        print("image processed")
 
-        cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
+        #transform pixel coordinates to camera frame
+        distance_to_floor = 0.7 #TODO get correct value from tf
+        weed_pose_array = PoseArray()
+        weed_pose_array.header.frame_id = '/map'
+        for i,weed_pixel_coord in enumerate(weed_pixel_coords):
+            weed_unit_vector = np.array(self.camera_model.projectPixelTo3dRay(weed_pixel_coord))
+            
+            #define a PoseStamped message of weed_pose in the camera frame
+            weed_pose = PoseStamped()
+            weed_pose.header.frame_id = self.camera_model.tfFrame()
+            weed_pose.pose.position.x,weed_pose.pose.position.y,weed_pose.pose.position.z = weed_unit_vector * distance_to_floor
+            
+            #transform weed_pose to the map frame
+            try: 
+                self.tf_listener.waitForTransform('/map', weed_pose.header.frame_id,rospy.Time(),rospy.Duration(0.5))
+                target_pose_WRT_map = self.tf_listener.transformPose('/map', weed_pose)
+            except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+                continue 
+            
+            #append transformed pose to weed_pose_array
+            weed_pose_array.poses.append(target_pose_WRT_map.pose)
+        
+        #publish pose_array
+        self.weed_pose_array_pub.publish(weed_pose_array)
 
+        #preparing and publishing image with added weed targets. 
+        #image should be rgb for display in rviz.
+        output_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
+        #plot a circle at each detected weed.
         if weed_pixel_coords is not None:
             for coord in weed_pixel_coords:
-                cv_image = cv2.circle(cv_image, tuple(coord), radius=10, color=(255, 0, 255), thickness=3)
-
+                output_image = cv2.circle(output_image, tuple(coord), radius=10, color=(255, 0, 255), thickness=3)
+        #publish output_image 
         try:
-            self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image))
+            self.image_pub.publish(self.bridge.cv2_to_imgmsg(output_image))
         except CvBridgeError as e:
             print(e)
 
@@ -142,6 +198,7 @@ if __name__ == '__main__':
     else:
         robot_name = "thorvald_001"
 
-    ic = image_converter()
     rospy.init_node('vision_node', anonymous=True)
+    ic = image_converter()
+    #s = rospy.Service('crop_and_weed_targets', AddTwoInts, handle_add_two_ints)
     rospy.spin()
